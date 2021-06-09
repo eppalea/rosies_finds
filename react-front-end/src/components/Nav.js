db53eee895da5fce30d5afbfbcdc807bd0ec479b
brodybayley <brodybayley@gmail.com>
2021-06-09 14:27:07 -0700

    Add nav bar css styling

--- react-front-end/src/components/Nav.js
+++ react-front-end/src/components/Nav.js
@@ -3,7 +3,13 @@ import logo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles(() => ({
+  navList: {
+    color: "#c38e70",
+    textDecoration: "none",
+    marginRight: "10px",
+  },
+}));
 
 export default function Nav() {
   const classes = useStyles();
