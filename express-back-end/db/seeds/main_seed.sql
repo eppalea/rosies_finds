83f23272a854a9f5f7677e1249633f00ba6ca0c3
Eppa Lea Turniawan <eppalea@gmail.com>
2021-06-09 14:25:19 -0700

    fix bugs in seed and table files

--- express-back-end/db/seeds/main_seed.sql
+++ express-back-end/db/seeds/main_seed.sql
@@ -1,7 +1,8 @@
 INSERT INTO users (first_name, last_name, email, password, admin)
-VALUES ('Sarah', 'Rosenkranz', 'rosiesfinds@outlook.com', 'Welcome1', TRUE);
+VALUES ('Sarah', 'Rosenkranz', 'rosiesfinds@outlook.com', 'Welcome1', TRUE),
+('Bradley', 'Beanie', 'bradley@beanworks.com', '123456', FALSE);
 
-INSERT INTO items (name, price, status, description, image, quantity, user_id)
+INSERT INTO items (name, price, current_status, description, image, quantity, user_id)
 VALUES ('blue vase', 20, 'available', '12" x 20"', 'www.wbtly.ca/s/LmXXzF', 1, 1),
 ('mirror', 45, 'available', '24" x 40"', 'www.wbtly.ca/s/dpvjxL', 1, 1),
 ('foxy after coffee mug', 5, 'available', 'cute mug', 'www.wbtly.ca/s/PU7ZOt', 4, 1);
