6deeac27d581d0bc35cb18ff732b96c451b139bc
brodybayley <brodybayley@gmail.com>
2021-06-09 14:06:36 -0700

    Add material ui to dependecies and add links to nav bar

--- react-front-end/src/components/Nav.js
+++ react-front-end/src/components/Nav.js
@@ -1,12 +1,31 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({}));
 
 export default function Nav() {
+  const classes = useStyles();
   return (
     <nav>
       <div className="logo">
         <img className="logo-img" src={logo} alt={logo} />
       </div>
+      <ul className="nav-links">
+        <Link className={classes.navList} to="/">
+          <li>Home</li>
+        </Link>
+        <Link className={classes.navList} to="/about">
+          <li>About</li>
+        </Link>
+        <Link className={classes.navList} to="/items">
+          <li>Items</li>
+        </Link>
+        <Link className={classes.navList} to="/sales">
+          <li>Sales</li>
+        </Link>
+      </ul>
     </nav>
   );
 }
