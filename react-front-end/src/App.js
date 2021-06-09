8057b23806f4e8a423162758753d251beee82552
brodybayley <brodybayley@gmail.com>
2021-06-09 12:58:28 -0700

    Add nav file with skeleton and add nav to app js

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -4,10 +4,12 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 //component imports
 import Items from "./components/Items";
+import Nav from "./components/Nav";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Switch>
         <Route path="/items" exact component={Items} />
       </Switch>
---
+++ react-front-end/src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <nav>
+      <h2>Hello</h2>
+    </nav>
+  );
+}
