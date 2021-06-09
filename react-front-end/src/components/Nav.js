9ba603d7549dc387d001c49085ee7ee89ce91c47
brodybayley <brodybayley@gmail.com>
2021-06-09 15:57:29 -0700

    Adjust styling and change items to household decor

--- react-front-end/src/components/Nav.js
+++ react-front-end/src/components/Nav.js
@@ -1,5 +1,6 @@
 import React from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo-image.png";
+import title from "../assets/company-title.png";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -17,19 +18,23 @@ export default function Nav() {
     <nav>
       <div className="logo">
         <img className="logo-img" src={logo} alt={logo} />
+        <img className="logo-img" src={title} alt={title} />
       </div>
       <ul className="nav-links">
         <Link className={classes.navList} to="/">
           <li>Home</li>
         </Link>
-        <Link className={classes.navList} to="/about">
-          <li>About</li>
-        </Link>
         <Link className={classes.navList} to="/items">
-          <li>Items</li>
+          <li>Household Decor</li>
         </Link>
         <Link className={classes.navList} to="/sales">
           <li>Sales</li>
+        </Link>
+        <Link className={classes.navList} to="/about">
+          <li>About</li>
+        </Link>
+        <Link className={classes.navList} to="/contact">
+          <li>Contact</li>
         </Link>
       </ul>
     </nav>
